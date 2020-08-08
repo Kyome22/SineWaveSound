e8292308fbbe8162c9a87549036390bbd020395b
Kyome22 <kyomesuke@gmail.com>
2020-08-09 05:12:34 +0900

    連打しても大丈夫！

--- SineWaveSound/SineWave.swift
+++ SineWaveSound/SineWave.swift
@@ -16,14 +16,22 @@ class SineWave {
         case out
     }
 
-    let audioEngine = AVAudioEngine()
-    let player = AVAudioPlayerNode()
     var volume: Float = 0
     var hz: Float = 0
+
+    private let audioEngine = AVAudioEngine()
+    private let player = AVAudioPlayerNode()
+    private var buffer: AVAudioPCMBuffer!
+    private var fadeInBuffer: AVAudioPCMBuffer!
+    private var fadeOutBuffer: AVAudioPCMBuffer!
 
     init(volume: Float = 0.1, hz: Float = 600) {
         self.volume = volume
         self.hz = hz
+
+        buffer = makeBuffer()
+        fadeInBuffer = makeBuffer(fade: .in)
+        fadeOutBuffer = makeBuffer(fade: .out)
 
         let audioFormat = player.outputFormat(forBus: 0)
         audioEngine.attach(player)
@@ -44,7 +52,7 @@ class SineWave {
         let audioFormat = player.outputFormat(forBus: 0)
         let sampleRate = Float(audioFormat.sampleRate) // 44100.0
         let length = AVAudioFrameCount(sampleRate / hz)
-        let capacity = fade == .none ? length : 50 * length
+        let capacity = fade == .none ? length : 20 * length
         guard let buf = AVAudioPCMBuffer(pcmFormat: audioFormat, frameCapacity: capacity) else {
             fatalError("Error initializing AVAudioPCMBuffer")
         }
@@ -57,26 +65,25 @@ class SineWave {
             case .in:   power = 0.5 * (1.0 - cosf(Float(n) * u))
             case .out:  power = 0.5 * (1.0 + cosf(Float(n) * u))
             }
-            let val = sinf(Float(n) * 2.0 * Float.pi / Float(length))
-            buf.floatChannelData?.advanced(by: 0).pointee[n] = power * volume * val
-            buf.floatChannelData?.advanced(by: 1).pointee[n] = power * volume * val
+            let value = power * volume * sinf(Float(n) * 2.0 * Float.pi / Float(length))
+            buf.floatChannelData?.advanced(by: 0).pointee[n] = value
+            buf.floatChannelData?.advanced(by: 1).pointee[n] = value
         }
         return buf
     }
 
     func play() {
         if audioEngine.isRunning {
-            player.stop()
             player.volume = 1.0
-            player.scheduleBuffer(makeBuffer(fade: .in), completionHandler: nil)
-            player.scheduleBuffer(makeBuffer(), at: nil, options: .loops, completionHandler: nil)
+            player.scheduleBuffer(fadeInBuffer, at: nil, options: .interrupts, completionHandler: nil)
+            player.scheduleBuffer(buffer, at: nil, options: .loops, completionHandler: nil)
             player.play()
         }
     }
 
     func pause() {
         if player.isPlaying {
-            player.scheduleBuffer(makeBuffer(fade: .out), at: nil, options: .interruptsAtLoop, completionHandler: {
+            player.scheduleBuffer(fadeOutBuffer, at: nil, options: .interruptsAtLoop, completionHandler: {
                 self.player.pause()
             })
             player.play()
@@ -85,7 +92,7 @@ class SineWave {
 
     func stop() {
         if player.isPlaying {
-            player.scheduleBuffer(makeBuffer(fade: .out), at: nil, options: .interruptsAtLoop, completionHandler: {
+            player.scheduleBuffer(fadeOutBuffer, at: nil, options: .interruptsAtLoop, completionHandler: {
                 self.player.stop()
             })
             player.play()
